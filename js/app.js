747fad6a1e0fd197d0b2ba816916a5659ceaaf19
ChzCharbel <charbel_chz@outlook.com>
2025-02-25 12:27:01 -0600

    laboratorio express app.js agregado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
-function cambiarTexto() {
-    document.getElementById("interests").innerHTML = "<h3>Hobbies</h3><p>En cuanto a mis hobbies, me gusta practicar todo tipo de deportes, en especial, el futbol, volibol y basquetbol, tambien me gusta jugar ajedrez y tocar la guitarra.</p>";
-}
+const express = require('express');
+const app = express();
+
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    response.send('¡Hola mundo!'); //Manda la respuesta
+});
+
+app.listen(3000);
+
---
+++ js/funcion.js
@@ -0,0 +1,3 @@
+function cambiarTexto() {
+    document.getElementById("interests").innerHTML = "<h3>Hobbies</h3><p>En cuanto a mis hobbies, me gusta practicar todo tipo de deportes, en especial, el futbol, volibol y basquetbol, tambien me gusta jugar ajedrez y tocar la guitarra.</p>";
+}

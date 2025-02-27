a6c954575d9429c6772b760a455eec64b2a29f1d
ChzCharbel <charbel_chz@outlook.com>
2025-02-27 12:59:06 -0600

    lab11

--- js/funcion.js
+++
@@ -1,3 +0,0 @@
-function cambiarTexto() {
-    document.getElementById("interests").innerHTML = "<h3>Hobbies</h3><p>En cuanto a mis hobbies, me gusta practicar todo tipo de deportes, en especial, el futbol, volibol y basquetbol, tambien me gusta jugar ajedrez y tocar la guitarra.</p>";
-}
